1dca677165fd15abe636c5e475270843f187f8b1
Johnson Tai <johnsont426@gmail.com>
2017-04-03 01:11:05 -0700

    add bag weight feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -61,5 +61,10 @@ class ApplicationController < Sinatra::Base
 			User.find(session[:user_id])
 		end
 
+		def bag_weight(bag)
+			lenses_weight = bag.lenses.collect{|lens| lens.weight}.sum
+			cameras_weight = bag.cameras.collect{|cameras| cameras.weight}.sum
+			lenses_weight + cameras_weight
+		end
 	end
 end
