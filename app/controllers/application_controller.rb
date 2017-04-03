282fb4b838e17ea2fc1d8583412dfaf2a4e85842
Johnson Tai <johnsont426@gmail.com>
2017-04-03 16:55:49 -0700

    move the login and signup action to the users controller and create users directory in views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,44 +13,6 @@ class ApplicationController < Sinatra::Base
 		erb :index
 	end
 
-	get '/signup' do
-		erb :signup
-	end
-
-	post '/signup' do
-		if params[:username] == "" || params[:password] == ""
-			flash[:message] = "Please enter username and password!"
-			redirect '/signup'
-		elsif params[:password].length < 8
-			flash[:message] = "Your password is too short!"
-			redirect '/signup'
-		else
-			user = User.new(params)
-			user.save
-			redirect '/login'
-		end
-
-	end
-
-	get '/login' do
-		erb :login
-	end
-
-	post '/login' do
-		@user = User.find_by(username: params[:username])
-		if !@user
-			flash[:message] = "Can't find the username"
-			redirect '/login'
-		elsif @user && !@user.authenticate(params[:password])
-			flash[:message] = "Wrong password"
-			redirect '/login'
-		else
-			session[:user_id] = @user.id
-			redirect '/bags'
-		end
-	end
-
-
 	helpers do
 
 		def logged_in?
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,40 @@
+class UsersController < ApplicationController
+
+	get '/signup' do
+		erb :'users/signup'
+	end
+
+	post '/signup' do
+		if params[:username] == "" || params[:password] == ""
+			flash[:message] = "Please enter username and password!"
+			redirect '/signup'
+		elsif params[:password].length < 8
+			flash[:message] = "Your password is too short!"
+			redirect '/signup'
+		else
+			user = User.new(params)
+			user.save
+			redirect '/login'
+		end
+
+	end
+
+	get '/login' do
+		erb :'users/login'
+	end
+
+	post '/login' do
+		@user = User.find_by(username: params[:username])
+		if !@user
+			flash[:message] = "Can't find the username"
+			redirect '/login'
+		elsif @user && !@user.authenticate(params[:password])
+			flash[:message] = "Wrong password"
+			redirect '/login'
+		else
+			session[:user_id] = @user.id
+			redirect '/bags'
+		end
+	end
+
+end
